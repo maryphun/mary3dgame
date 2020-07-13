b3e0b40808797c81424eb8ab29da8030e4347afd
Mary <phunmung@gmail.com>
2020-07-13 20:31:03 +0900

    animation
    a

--- Assets/thirdpersonmovement.cs
+++ Assets/thirdpersonmovement.cs
@@ -6,6 +6,7 @@ public class thirdpersonmovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform camera;
+    public Animator animator;
 
     public float speed = 0.001f;
     public float turnSmoothTime = 0.01f;
@@ -13,8 +14,24 @@ public class thirdpersonmovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float moveSpeed = speed;
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift) && vertical == 1f)
+        {
+            vertical *= 2f;
+            moveSpeed *= 2f;
+        }
+        if (vertical == -1)
+        {
+            moveSpeed /= 2;
+        }
+
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Horizontal", horizontal);
+        Debug.Log(vertical.ToString());
+
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
@@ -27,7 +44,7 @@ public class thirdpersonmovement : MonoBehaviour
         {
             float moveTargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             Vector3 moveDirection = Quaternion.Euler(0f, moveTargetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
         }
     }
 }
